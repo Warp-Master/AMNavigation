130a2e3b3feed7a41eaf37a8a7e841f2be6f0b8b
Dmitrii Chernov <2003dima@gmail.com>
2022-10-23 00:01:33 +0300

    переписан класс Flight

--- Flight.py
+++ Flight.py
@@ -9,21 +9,30 @@ from config import DEFAULT_DISTANCE
 
 @dataclass
 class Flight:
-    id: int
-    date: datetime
-    flight_type: str
-    terminal_name: str
-    airline_code: str
-    airline_number: int
-    airport_code: str
-    airport_name: str
-    aircraft_type: str
-    parking_number: str
-    gate_number: str
-    passengers_count: int
+    def __init__(self, flight_id: int, date: datetime, flight_type: str, terminal_name: str, airline_code: str,
+                 airline_number: int, airport_code: str, airport_name: str, aircraft_type: str, parking_number: str,
+                 gate_number: str, passengers_count: int) -> None:
+        self.id: int = flight_id
+        self.date: datetime = date
+        self.flight_type: str = flight_type
+        self.terminal_name: str = terminal_name
+        self.airline_code: str = airline_code
+        self.airline_number: int = airline_number
+        self.airport_code: str = airport_code
+        self.airport_name: str = airport_name
+        self.aircraft_type: str = aircraft_type
+        self.parking_number: str = parking_number
+        self.gate_number: str = gate_number
+        self.passengers_count: int = passengers_count
 
     def __str__(self):
         return f"{self.airline_code}-{self.airline_number}"
+
+    def __lt__(self, other: "Flight") -> bool:
+        return self.date < other.date
+
+    def __gt__(self, other: "Flight") -> bool:
+        return self.date > other.date
 
     def get_distance(self, g: Graph, cache: Cache, mapping: NameMap) -> float:
         if self.flight_type == "A":
